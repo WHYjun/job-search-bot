6c0ec09baab3ce9cfb6662c4bc794ed56efcd571
WHYjun <yjunchoi93@gmail.com>
2018-01-24 22:21:41 -0500

    Add a case for iframe tag

--- jobsearch.py
+++ jobsearch.py
@@ -10,7 +10,7 @@ class JobSearch:
         self.reader = reader
         self.company = []
         self.urls = []
-        self.classname = []
+        self.name = []
         self.original = []
         self.contact = []
         self.update = []
@@ -21,12 +21,12 @@ class JobSearch:
             self.update.append(line)
             self.company.append(line[0])
             self.urls.append(line[1])
-            self.classname.append(line[2])
+            self.name.append(line[2])
             self.original.append(line[3])
             self.contact.append(line[4])
 
-    def write(self):
-         with open('local/userinfo.csv', 'w') as f:
+    def write(self, filename):
+         with open(filename, 'w') as f:
              writer = csv.writer(f)
              for i in range(len(self.update)):
                 writer.writerow(self.update[i])
@@ -34,19 +34,32 @@ class JobSearch:
     def check(self):
         self.export()
         for i in range(len(self.company)):
-            req = urllib.request.urlopen(self.urls[i])
+            req = urllib.request.Request(self.urls[i], headers={'User-Agent': 'Mozilla/5.0'})
+            req = urllib.request.urlopen(req).read()
             soup = BeautifulSoup(req, 'html.parser')
-            result = soup.find(class_= self.classname[i])
+            result = self.find(soup, self.name[i], i)
             word = str(result.get_text("", strip = True))
             if word != self.original[i]:
-                print("A new opening " + word + " at " + self.company[i])
-                self.update[i][3] = word
+                print("A new opening at " + self.company[i])
+                self.update[i+1][3] = word
             else:
                 print("No Update at " + self.company[i])
-        self.write()
+    
+    def find(self, soup, name, i):
+        if name.startswith('iframe'):
+            iframe = soup.find('iframe')
+            response = urllib.request.Request(iframe.attrs['src'], headers={'User-Agent': 'Mozilla/5.0'})
+            response = urllib.request.urlopen(response).read()
+            iframe_soup = BeautifulSoup(response)
+            return iframe_soup.find(class_ = self.name[i][6:])
+        else:
+            return soup.find(class_= self.name[i])
+
 
 if __name__ == '__main__':
-    with open('local/userinfo.csv', 'r', encoding = 'utf-8') as f:
+    filename = 'local/userinfo.csv'
+    with open(filename, 'r', encoding = 'utf-8') as f:
         reader = csv.reader(f)
         search = JobSearch(reader)
         search.check()
+    search.write(filename)
