acb60b7436e163fa61f1a21d7b8030810d2bfa36
WHYjun <yjunchoi93@gmail.com>
2018-03-07 18:21:54 -0500

    Debug accessDB module

--- jobbot/jobbot/spiders/accessDB.py
+++ jobbot/jobbot/spiders/accessDB.py
@@ -1,28 +1,38 @@
 # -*- coding: utf-8 -*-
 import pymongo
 import json
+import os.path
 
-class accessDB:
-    with open('config.json', 'r') as f:
-        datastore = json.load(f)
+scriptpath = os.path.dirname(__file__)
+filename = os.path.join(scriptpath, 'config.json')
+with open(filename) as f:
+    datastore = json.load(f)
+
+name = datastore['user']['name']
+pwd = datastore['user']['pwd']
+
+def compare(company,urls):
+    client = pymongo.MongoClient()
+    repo = client.repo
+    repo.authenticate(name, pwd)
+
+    collectionName = company + 'list'
+    prevLst = repo[name + '.' + collectionName].find()
     
-    name = datastore[user][name]
-    pwd = datastore[user][pwd]
+    lst = []
+    for url in urls:
+        lst.append(str(url))
 
-    def compare(self,company,lst):
-        client = pymongo.MongoClient()
-        repo = client.repo
-        repo.authenticate(name, pwd)
+    # Debugging only
+    # with open(os.path.join(scriptpath, 'debug.txt'), 'w') as d:
+    #     for i in lst:
+    #         d.write(i)
+    #         d.write('\n')
 
-        collectionName = '.' + company + 'list'
-        prevLst = repo[name + collectionName].find()
-        
-        for i in lst:
-            if i not in prevLst:
-                repo.dropCollection(collectionName)
-                repo.createCollection(collectionName)
-                return True
-            else:
-                return False
-
-/* eof */
+    for i in lst:
+        if i not in prevLst:
+            repo.drop_collection(collectionName)
+            repo.create_collection(collectionName)
+            return True
+        else:
+            return False
--- jobbot/jobbot/spiders/pewresearch.py
+++ jobbot/jobbot/spiders/pewresearch.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
 from scrapy.linkextractors import LinkExtractor
 import scrapy
+import os
+import sys
+
+scriptpath = os.path.dirname(__file__)
+sys.path.append(os.path.abspath(scriptpath))
 from accessDB import compare
 
 class PewresearchSpider(scrapy.Spider):
     name = 'pewresearch'
-    allowed_domains = ['https://jobs-prc.icims.com/']
+    allowed_domains = ['jobs-prc.icims.com/']
     start_urls = ['https://jobs-prc.icims.com/jobs/search?ss=1&searchLocation=&searchCategory=&in_iframe=1&hashed=124493942']
 
     def parse(self, response):
+        name = 'pewresearch'
         lx = LinkExtractor()
         lst = lx.extract_links(response) # List contains the list of jobs
-        # @TODO Call the function which compares between lst and MongoDB. Return Boolean Value
+        # Call the function which compares between lst and MongoDB. Return Boolean Value
         flag = compare(name,lst)
         # @TODO if True, call the function which send an email to users
-        
