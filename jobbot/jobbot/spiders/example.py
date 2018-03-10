b9ff12c72733ac5e07bcfd6f4d518aa8add02659
WHYjun <yjunchoi93@gmail.com>
2018-03-10 18:14:56 -0500

    Upload example.py

--- jobbot/jobbot/spiders/example.py
+++ jobbot/jobbot/spiders/example.py
@@ -9,25 +9,19 @@ sys.path.append(os.path.abspath(scriptpath))
 from accessDB import compare
 from sendEmail import notify
 
-class PewresearchSpider(scrapy.Spider):
-    name = 'pewresearch'
-    allowed_domains = ['jobs-prc.icims.com/']
-    start_urls = ['https://jobs-prc.icims.com/jobs/search?ss=1&searchLocation=&searchCategory=&in_iframe=1&hashed=124493942']
+class ExampleSpider(scrapy.Spider):
+    name = 'example'
+    allowed_domains = ['example.com/']
+    start_urls = ['https://example.com/career']
 
     def parse(self, response):
-        name = 'pewresearch'
+        name = 'example'
         lx = LinkExtractor()
-        urls = lx.extract_links(response) # List contains the list of jobs
-        lst = []
-        for url in urls:
-            jobtitle = str(url.text)
-            if "\nJob Title\n\n" in jobtitle:
-                lst.append(jobtitle[12:])
+        lst = lx.extract_links(response) # List contains the list of jobs
         # Call the function which compares between lst and MongoDB. Return Boolean Value
         flag = compare(name,lst)
         # if True, call the function which send an email to users
         if flag:
             notify(name)
-            print("Notified")
         else:
             print("No Update")
